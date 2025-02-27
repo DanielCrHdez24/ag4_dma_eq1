d0e287f5efad455c6b6766f99fd8a17c15cf438d
Daniel Cruz Hernandez <a22300104@unideh.edu.mx>
2025-02-27 15:17:35 -0600

    actualizo firebase_api

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -3,9 +3,22 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
+  // Corrige el nombre del parámetro 'message' para que sea 'RemoteMessage message'
+  Future<void> handleBackgroundMessage(RemoteMessage message) async {
+    print('Title: ${message.notification?.title}');
+    print('Body: ${message.notification?.body}');
+    print('Payload: ${message.data}');
+  }
+
   Future<void> initNotifications() async {
+    // Solicitar permisos de notificación
     await _firebaseMessaging.requestPermission();
+
+    // Obtener el token FCM
     final fCMToken = await _firebaseMessaging.getToken();
-    print ('Token: $fCMToken');
+    print('Token: $fCMToken');
+
+    // Establecer el manejador de mensajes en segundo plano
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   }
 }
