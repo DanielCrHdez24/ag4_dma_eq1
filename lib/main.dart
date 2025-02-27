d673e5655b09acf91efe7d0f36e799eb37c53ca0
a22300149 <a22300149@unideh.edu.mx>
2025-02-26 22:33:27 -0600

    Update main.dart
    Comentario

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class App extends StatelessWidget {
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: LoginScreen(),//
     );
   }
 }
